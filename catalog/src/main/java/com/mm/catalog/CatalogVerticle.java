f308c76c39c086bed132d27b92e807c315ff1d3a
Sergio López <sergio.lopezdelaflor@masmovil.com>
2021-02-24 09:10:21 +0100

    Catalog verticle poc done

--- catalog/src/main/java/com/mm/catalog/CatalogVerticle.java
+++ catalog/src/main/java/com/mm/catalog/CatalogVerticle.java
@@ -1,23 +1,38 @@
 package com.mm.catalog;
 
-import io.vertx.core.AbstractVerticle;
+import io.reactivex.Maybe;
+import io.reactivex.Single;
+import io.vertx.core.Future;
+import io.vertx.core.json.Json;
+import io.vertx.core.json.JsonObject;
+import io.vertx.reactivex.core.AbstractVerticle;
 import io.vertx.core.Promise;
+import io.vertx.core.impl.logging.Logger;
+import io.vertx.core.impl.logging.LoggerFactory;
+import io.vertx.reactivex.core.http.HttpServer;
+import io.vertx.reactivex.ext.web.Router;
 
 public class CatalogVerticle extends AbstractVerticle {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(CatalogVerticle.class);
+
   @Override
-  public void start(Promise<Void> startPromise) throws Exception {
-    vertx.createHttpServer().requestHandler(req -> {
-      req.response()
-        .putHeader("content-type", "text/plain")
-        .end("Hello from Vert.x!");
-    }).listen(8888, http -> {
-      if (http.succeeded()) {
-        startPromise.complete();
-        System.out.println("HTTP server started on port 8888");
-      } else {
-        startPromise.fail(http.cause());
-      }
+  public void start(final Promise<Void> startPromise) {
+
+    HttpServer httpServer = vertx.createHttpServer();
+
+    Router router = Router.router(vertx);
+
+    router.get("/catalog").handler(ctx -> {
+      ctx.response()
+        .putHeader("Content-Type", "application/json")
+        .end(Json.encodePrettily(new JsonObject().put("msg", "hola")));
     });
+
+    httpServer.requestHandler(router)
+      .rxListen(8080)
+      .subscribe(server -> LOGGER.info(String.format("Server listening on port %s", server.actualPort())),
+        failure -> LOGGER.error(failure.getMessage()));
+
   }
 }
